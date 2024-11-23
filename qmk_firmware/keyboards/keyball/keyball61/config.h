ab8a08b86ef05f92893eaeeb1ccb2aa287f8c604
yqYo1 <miyasyrog@gmail.com>
2024-11-24 03:21:48 +0900

    Retern to change

--- qmk_firmware/keyboards/keyball/keyball61/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/config.h
@@ -69,9 +69,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define OLED_FONT_END 195
 #endif
 
-// #if !defined(LAYER_STATE_8BIT) && !defined(LAYER_STATE_16BIT) && !defined(LAYER_STATE_32BIT)
-// #    define LAYER_STATE_8BIT
-// #endif
+#if !defined(LAYER_STATE_8BIT) && !defined(LAYER_STATE_16BIT) && !defined(LAYER_STATE_32BIT)
+#    define LAYER_STATE_8BIT
+#endif
 
 // To squeeze firmware size
 #undef LOCKING_SUPPORT_ENABLE
