cd7ddf234ae090d155deacaacd797a97a86257ce
yqYo1 <miyasyrog@gmail.com>
2024-12-04 22:30:27 +0900

    remove unused code

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/custom/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/custom/config.h
@@ -24,6 +24,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 2
-//#undef LAYER_STATE_8BIT
-//#define LAYER_STATE_16BIT
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
