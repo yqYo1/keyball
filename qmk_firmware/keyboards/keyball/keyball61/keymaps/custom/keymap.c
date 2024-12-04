da91e5465d5951830499ffab543431857044cb04
yqYo1 <miyasyrog@gmail.com>
2024-12-04 23:47:46 +0900

    AML default enable on custom keymap

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/custom/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/custom/keymap.c
@@ -72,3 +72,10 @@ void oledkit_render_info_user(void) {
     keyball_oled_render_layerinfo();
 }
 #endif
+
+// AML default enable
+#ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
+void pointing_device_init_user(void) {
+    set_auto_mouse_enable(true);
+}
+#endif
