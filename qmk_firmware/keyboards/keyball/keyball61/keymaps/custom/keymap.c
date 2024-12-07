4ae1b76972735e9128e72a0e9fae84c13e87cabc
yaYoi <52419113+yqYo1@users.noreply.github.com>
2024-12-07 23:56:23 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/custom/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/custom/keymap.c
@@ -82,8 +82,8 @@ void oledkit_render_info_user(void) {
 #endif
 
 // AML default enable
-#ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
+//#ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
 void pointing_device_init_user(void) {
     set_auto_mouse_enable(true);
 }
-#endif
+//#endif
