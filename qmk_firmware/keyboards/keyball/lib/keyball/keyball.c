e63390526db03998512ba24394cc1e23f3ce1d80
yqYo1 <miyasyrog@gmail.com>
2024-12-09 00:22:53 +0900

    update keyball.c

--- qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
+++ qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
@@ -581,7 +581,8 @@ void keyboard_post_init_kb(void) {
         keyball_set_cpi(c.cpi);
         keyball_set_scroll_div(c.sdiv);
 #ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
-        set_auto_mouse_enable(c.amle);
+        /* set_auto_mouse_enable(c.amle); */
+        set_auto_mouse_enable(true);
         set_auto_mouse_timeout(c.amlto == 0 ? AUTO_MOUSE_TIME : (c.amlto + 1) * AML_TIMEOUT_QU);
 #endif
 #if KEYBALL_SCROLLSNAP_ENABLE == 2
